2f2c0e9d75cd4071e3d135bed906c320c874b242
Benjamin Dow <ben@bonta-kun.net>
2012-08-18 20:11:37 -0500

    Cleaned up the package.json, added an example usage.

---
+++ example/simpleExample.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+//grab a reference to the library, this is simplier when you're using it as a library.
+var steamServerStatus = require(__dirname + '/../lib/steamServerStatus');
+
+steamServerStatus.getServerStatus(
+	'tf.bonta-kun.net', 27015, function(serverInfo) {
+		console.log("game: " + serverInfo.gameName);
+		console.log("server name: " + serverInfo.serverName);
+		console.log("players: " + serverInfo.numberOfPlayers + "/" + serverInfo.maxNumberOfPlayers)
+});
